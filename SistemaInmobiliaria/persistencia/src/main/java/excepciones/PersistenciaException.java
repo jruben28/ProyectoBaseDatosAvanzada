b8b6c603ffd683000d26a59f69f8bc5fa8239d57
jruben28 <140290443+jruben28@users.noreply.github.com>
2026-05-13 20:30:12 -0700

    clase persistencia exception

---
+++ SistemaInmobiliaria/persistencia/src/main/java/excepciones/PersistenciaException.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package excepciones;
+
+/**
+ * Excepción personalizada para representar errores ocurridos en la capa
+ * de persistencia.
+ * @author joser
+ */
+public class PersistenciaException extends Exception {
+
+    public PersistenciaException(String mensaje) {
+        super(mensaje);
+    }
+
+    public PersistenciaException(String mensaje, Throwable causa) {
+        super(mensaje, causa);
+    }
+}
