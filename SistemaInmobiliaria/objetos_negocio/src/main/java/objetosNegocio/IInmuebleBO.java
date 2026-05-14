9053638dd6cc3172da2c89559a379d845043a168
jruben28 <140290443+jruben28@users.noreply.github.com>
2026-05-14 02:25:29 -0700

    Update IInmuebleBO.java

--- SistemaInmobiliaria/objetos_negocio/src/main/java/objetosNegocio/IInmuebleBO.java
+++ SistemaInmobiliaria/objetos_negocio/src/main/java/objetosNegocio/IInmuebleBO.java
@@ -4,10 +4,41 @@
  */
 package objetosNegocio;
 
+import dtos.comunes.InmuebleDTO;
+import dtos.comunes.InquilinoDTO;
+import dtos.entrada.RegistrarInmuebleDTO;
+import dtos.salida.InmuebleSalidaDTO;
+import excepcion.NegocioException;
+import java.util.List;
+
 /**
  *
  * @author joser
  */
-public interface IInmuebleBO {
+public interface IInmuebleBO{
+    
+    /**
+     * Lista inmuebles con estado en disponible
+     * @return lista con inmuebles sin inquilino 
+     * @throws excepcion.NegocioException 
+     */
+    List<InmuebleSalidaDTO> listarInmueblesDisponibles() throws NegocioException;
+    
+    /**
+     * Obtiene el inmueble buscado según su id.
+     * @param idInmueble
+     * @return 
+     * @throws excepcion.NegocioException 
+     */
+    public InmuebleSalidaDTO buscarInmueblePorId(String idInmueble) throws NegocioException;
+    
+    /**
+     * Registra un inquilino embebido al inmueble y cambia el estado. 
+     * @param idInmueble
+     * @param inquilinoDTO
+     * @return 
+     * @throws excepcion.NegocioException 
+     */
+    public boolean registrarInquilino(String idInmueble, InquilinoDTO inquilinoDTO) throws NegocioException;
     
 }
